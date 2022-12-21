c1d33fc481954cd732719a61346b6c5824f41097
Neil Stoker <neil.stoker@gmail.com>
2022-12-21 22:13:09 +0000

    use app router package

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	router "github.com/nstoker/stoker.dev/pkg/router"
 	"github.com/nstoker/stoker.dev/pkg/version"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	log.Infof("Starting up %s", version.Version())
+	_, err := router.GetRouter()
+	if err != nil {
+		logrus.Fatalf("router init failed %v", err)
+	}
+
+	log.Infof(router.Run("127.0.0.1:3000").Error())
 }
